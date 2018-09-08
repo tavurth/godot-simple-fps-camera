1ea059c0739a7fc23d6b9cbe5fcb48b1a81b756b
will <will.whitty.arbeit@gmail.com>
2018-09-08 19:22:44 +0300

    Added some comments

--- Camera.gd
+++ Camera.gd
@@ -11,7 +11,13 @@ func look_updown_rotation(rotation = 0):
 	Get the new rotation for looking up and down
 	"""
 	var toReturn = self.get_rotation() + Vector3(rotation, 0, 0)
+
+	##
+	## We don't want the player to be able to bend over backwards
+	## neither to be able to look under their arse.
+	## Here we'll clamp the vertical look to 90° up and down
 	toReturn.x = clamp(toReturn.x, PI / -2, PI / 2)
+
 	return toReturn
 
 func look_leftright_rotation(rotation = 0):
@@ -24,7 +30,15 @@ func mouse(event):
 	"""
 	First person camera controls
 	"""
+	##
+	## We'll use the parent node "Yaw" to set our left-right rotation
+	## This prevents us from adding the x-rotation to the y-rotation
+	## which would result in a kind of flight-simulator camera
 	Yaw.set_rotation(look_leftright_rotation(event.relative.x / -200))
+
+	##
+	## Now we can simply set our y-rotation for the camera, and let godot
+	## handle the transformation of both together
 	self.set_rotation(look_updown_rotation(event.relative.y / -200))
 
 func _input(event):
