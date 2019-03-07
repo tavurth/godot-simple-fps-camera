d03d5ef0d89176dbbea5b120bd810b7c296380c0
will <will.whitty.arbeit@gmail.com>
2019-03-07 16:59:22 +0300

    Use absolute mouse position so that we can use _process

--- Camera.gd
+++ Camera.gd
@@ -5,9 +5,14 @@ onready var Yaw = get_parent()
 ## Increase this value to give a slower turn speed
 const CAMERA_TURN_SPEED = 200
 
+onready var lastPosition = get_viewport().get_mouse_position()
+
+func get_mouse_position():
+	return get_viewport().get_mouse_position()
+
 func _ready():
 	## Tell Godot that we want to handle input
-	set_process_input(true)
+	set_process(true)
 
 func look_updown_rotation(rotation = 0):
 	"""
@@ -31,7 +36,7 @@ func look_leftright_rotation(rotation = 0):
 	"""
 	return Yaw.get_rotation() + Vector3(0, rotation, 0)
 
-func mouse(event):
+func updateMousePosition(mouseMotion):
 	"""
 	First person camera controls
 	"""
@@ -39,18 +44,28 @@ func mouse(event):
 	## We'll use the parent node "Yaw" to set our left-right rotation
 	## This prevents us from adding the x-rotation to the y-rotation
 	## which would result in a kind of flight-simulator camera
-	Yaw.set_rotation(look_leftright_rotation(event.relative.x / -CAMERA_TURN_SPEED))
+	Yaw.set_rotation(look_leftright_rotation(mouseMotion.x / -CAMERA_TURN_SPEED))
 
 	##
 	## Now we can simply set our y-rotation for the camera, and let godot
 	## handle the transformation of both together
-	self.set_rotation(look_updown_rotation(event.relative.y / -CAMERA_TURN_SPEED))
+	self.set_rotation(look_updown_rotation(mouseMotion.y / -CAMERA_TURN_SPEED))
 
-func _input(event):
+func _process(delta):
 	##
-	## We'll only process mouse motion events
-	if event is InputEventMouseMotion:
-		return mouse(event)
+	## We're using _process because in a FPS game
+	## Often the user has a very powerful computer
+	## It will be cheaper in the long run to simply process every frame
+	## than to try and handle all MouseMotion events
+	var newPosition = get_mouse_position()
+	
+	## Update the users viewport
+	updateMousePosition(newPosition - lastPosition)
+	
+	## Saving the current mouse position
+	## Now we can calculate the difference on the next frame
+	## and so understand how much we should move the pointer
+	self.lastPosition = newPosition
 
 func _enter_tree():
 	"""
