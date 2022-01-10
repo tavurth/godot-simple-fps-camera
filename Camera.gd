0b05e34381812d91d4ba30dacb2751d16d634fc8
Will Whitty <tavurth@gmail.com>
2022-01-10 12:43:08 +0300

    Ensure working with Godot 3.4.2

--- Camera.gd
+++ Camera.gd
@@ -11,8 +11,8 @@ func _ready():
 
 func look_updown_rotation(rotation = 0):
 	"""
-  Returns a new Vector3 which contains only the x direction
-  We'll use this vector to compute the final 3D rotation later
+	Returns a new Vector3 which contains only the x direction
+	We'll use this vector to compute the final 3D rotation later
 	"""
 	var toReturn = self.get_rotation() + Vector3(rotation, 0, 0)
 
@@ -26,15 +26,20 @@ func look_updown_rotation(rotation = 0):
 
 func look_leftright_rotation(rotation = 0):
 	"""
-  Returns a new Vector3 which contains only the y direction
-  We'll use this vector to compute the final 3D rotation later
+	Returns a new Vector3 which contains only the y direction
+	We'll use this vector to compute the final 3D rotation later
 	"""
 	return Yaw.get_rotation() + Vector3(0, rotation, 0)
 
-func mouse(event):
+func _input(event):
 	"""
 	First person camera controls
 	"""
+	##
+	## We'll only process mouse motion events
+	if not event is InputEventMouseMotion:
+		return
+
 	##
 	## We'll use the parent node "Yaw" to set our left-right rotation
 	## This prevents us from adding the x-rotation to the y-rotation
@@ -45,12 +50,6 @@ func mouse(event):
 	## Now we can simply set our y-rotation for the camera, and let godot
 	## handle the transformation of both together
 	self.set_rotation(look_updown_rotation(event.relative.y / -CAMERA_TURN_SPEED))
-
-func _input(event):
-	##
-	## We'll only process mouse motion events
-	if event is InputEventMouseMotion:
-		return mouse(event)
 
 func _enter_tree():
 	"""
