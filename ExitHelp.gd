84bf87d6313bec01832e51bbb85fdcd829a4379d
Will Whitty <tavurth@gmail.com>
2022-02-06 22:57:27 +0300

    Cleanup and add flashlight

--- ExitHelp.gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-func _ready():
-	set_process_input(true)
-
-func _input(event):
-	if event is InputEventKey:
-		if Input.is_action_pressed("ui_exit"):
-			get_tree().quit()
---
+++ player/Flashlight.gd
@@ -0,0 +1,8 @@
+extends SpotLight
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("action_flashlight"):
+		self.set_visible(!self.visible)
+		
+		$Sound.pitch_scale = 1.1 if self.visible else 0.9
+		$Sound.play()
