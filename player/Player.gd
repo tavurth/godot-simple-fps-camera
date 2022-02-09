5e05291a7092ee7e6a5a9d47e188ca1ddaff11d5
Will Whitty <tavurth@gmail.com>
2022-02-09 14:24:52 +0300

    Latest

--- player/Player.gd
+++ player/Player.gd
@@ -1,47 +1,81 @@
 extends KinematicBody
 
 var velocity = Vector3(0, 0, 0)
+var direction = Vector3(0, 0, 0) # Used for animation
 
-const JUMP = 5
+const JUMP = 4
 const PLAYER_MOVE_SPEED = 4
-onready var GRAVITY = ProjectSettings.get("physics/3d/default_gravity") / 1000
 
-func move_forward_back(delta: float):
+onready var Camera = $Camera
+onready var GRAVITY = ProjectSettings.get("physics/3d/default_gravity") / 1000
+onready var animation_tree = $character/AnimationTree
+
+func move_forward_back(in_direction: int):
 	"""
 	Move the camera forward or backwards
 	"""
-	self.transform = self.transform.translated(Vector3(0, 0, delta * PLAYER_MOVE_SPEED))
+	self.direction.z += in_direction
+	self.velocity += get_transform().basis.z * in_direction
 
-func move_left_right(delta: float):
+func move_left_right(in_direction: int):
 	"""
 	Move the camera to the left or right
 	"""
-	self.transform = self.transform.translated(Vector3(delta * PLAYER_MOVE_SPEED, 0, 0))
+	self.direction.x += in_direction
+	self.velocity += get_transform().basis.x * in_direction
 
-func _process(delta: float):
+func blend_animations():
+	print(direction)
+	animation_tree["parameters/run/blend_position"] = -direction.z
+	animation_tree["parameters/strafe/blend_position"] = direction.x
+
+func _process(_delta: float):
 	"""
 	Allow the player to move the camera with WASD
 	See Project settings -> Input map for keyboard bindings
 	"""
+	# Preserve the Y velocity from the previous frame
+	self.velocity = Vector3(0, self.velocity.y, 0)
+	self.direction = Vector3(0, 0, 0)
+	
 	if Input.is_action_pressed("ui_up"):
-		self.move_forward_back(-delta)
+		self.move_forward_back(-1)
 
 	elif Input.is_action_pressed("ui_down"):
-		self.move_forward_back(+delta)
+		self.move_forward_back(+1)
 
 	if Input.is_action_pressed("ui_left"):
-		self.move_left_right(-delta)
+		self.move_left_right(-1)
 
 	elif Input.is_action_pressed("ui_right"):
-		self.move_left_right(+delta)
+		self.move_left_right(+1)
+	
+	# new_velocity = new_velocity.normalized() * PLAYER_MOVE_SPEED
+	# new_velocity.y = velocity.y
+	# self.velocity = new_velocity
 
-func _physics_process(delta: float):
+func _physics_process(_delta: float):
+	var snap_vector = Vector3(0, -1, 0)
+	
 	if Input.is_action_just_pressed("action_jump"):
 		if self.is_on_floor():
 			self.velocity.y += JUMP
+			# Disable snap to floor for the jumping frame
+			snap_vector = Vector3(0, 0, 0)
+	
+	# Apply less gravity if we were on the floor last frame
+	# This helps our KinematicBody to avoid physics jitter
+	if self.is_on_floor():
+		self.velocity -= Vector3(0, GRAVITY / 100, 0)
+	else:
+		self.velocity -= Vector3(0, GRAVITY, 0)
 
-	self.velocity -= Vector3(0, GRAVITY, 0)
-	self.velocity.y = clamp(self.velocity.y, -GRAVITY * 100, GRAVITY * 100)
+	# Play the run/strafe/idle animation
+	self.blend_animations()
 
-	self.velocity = self.move_and_slide(self.velocity, Vector3(0, 1, 0))
-	self.transform = self.transform.translated(self.velocity * delta)
+	self.velocity = self.move_and_slide_with_snap(
+		self.velocity,
+		snap_vector,
+		Vector3(0, +1, 0),
+		true
+	)
